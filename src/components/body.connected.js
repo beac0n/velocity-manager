59107e2fa55c98d6489fb93e547504a73ae0700f
beac0n <mcschempp@gmx.de>
2016-11-27 19:38:22 +0100

    start implementing body table

--- src/components/body.connected.js
+++ src/components/body.connected.js
@@ -3,38 +3,43 @@ import {connect} from 'react-redux'
 import {Table} from 'reactstrap'
 import {selectors} from '../redux/reducer'
 
-const Body = ({sprintWeeks, sprintStart, sprintEnd}) => (
-    <Table>
-        <thead>
-        <tr>
-            <th>Benutzer</th>
-            <th>Montag</th>
-            <th>Dienstag</th>
-            <th>Mittwoch</th>
-            <th>Donnerstag</th>
-            <th>Freitag</th>
-            <th>Samstag</th>
-            <th>Sonntag</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td>Max</td>
-            <td>FULL</td>
-            <td>X</td>
-            <td>/</td>
-            <td>FULL</td>
-            <td>FULL</td>
-            <td>{sprintStart}</td>
-            <td>{sprintEnd}</td>
-        </tr>
-        </tbody>
-    </Table>)
+const Body = ({sprintWeeks, sprintStart, sprintEnd}) => {
+    if(!sprintStart || !sprintEnd) {
+        return null
+    }
+
+    return (
+        <Table>
+            <thead>
+            <tr>
+                <th>Benutzer</th>
+                <th>{sprintStart}</th>
+                <th>Dienstag</th>
+                <th>Mittwoch</th>
+                <th>Donnerstag</th>
+                <th>Freitag</th>
+                <th>Samstag</th>
+                <th>{sprintEnd}</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Max</td>
+                <td>FULL</td>
+                <td>X</td>
+                <td>/</td>
+                <td>FULL</td>
+                <td>FULL</td>
+                <td>X</td>
+                <td>X</td>
+            </tr>
+            </tbody>
+        </Table>)
+}
 
 const mapStateToProps = (state) => ({
     sprintStart: selectors.getSprintBoundary(state, 'sprintStart'),
     sprintEnd: selectors.getSprintBoundary(state, 'sprintEnd'),
 })
-const mapActionsToProps = {
-}
+const mapActionsToProps = {}
 export default connect(mapStateToProps, mapActionsToProps)(Body)
