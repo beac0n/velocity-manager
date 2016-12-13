a825a23e2ccb58d7dd9b72a6731257808d3f9959
beac0n <mcschempp@gmx.de>
2016-12-13 17:00:22 +0100

    add tests

--- src/redux/reducer/body.test.js
+++ src/redux/reducer/body.test.js
@@ -28,14 +28,40 @@ describe('body reducer', () => {
     })
 
     describe('event', () => {
+        it('should have no initial events', () => {
+            defaultStore.dispatch(actions.addUser('TestUser'))
+
+            expect(selectors.getEvents(defaultStore.getState(), 'TestUser', 0)).toEqual([])
+        })
+
         it('should add an event', () => {
             defaultStore.dispatch(actions.addUser('TestUser'))
             defaultStore.dispatch(actions.addEvent({username: 'TestUser', columnId: 0, begin: 0, end: 1}))
 
             expect(selectors.getEvents(defaultStore.getState(), 'TestUser', 0)).toContainEqual({begin: 0, end: 1})
+        })
 
+        it('should add an event which is only one hour long', () => {
+            defaultStore.dispatch(actions.addUser('TestUser'))
+            defaultStore.dispatch(actions.addEvent({username: 'TestUser', columnId: 0, begin: 0}))
+
+            expect(selectors.getEvents(defaultStore.getState(), 'TestUser', 0)).toContainEqual({begin: 0, end: 1})
+        })
+
+        it('should update an event with a note', () => {
+            defaultStore.dispatch(actions.addUser('TestUser'))
+            defaultStore.dispatch(actions.addEvent({username: 'TestUser', columnId: 0, begin: 0, end: 1}))
+            defaultStore.dispatch(actions.updateEvent({username: 'TestUser', columnId: 0, index: 0, note: 'testNote'}))
+
+            expect(selectors.getEvents(defaultStore.getState(), 'TestUser', 0)).toContainEqual({begin: 0, end: 1, note: 'testNote'})
+        })
+
+        it('should remove an event', () => {
+            defaultStore.dispatch(actions.addUser('TestUser'))
+            defaultStore.dispatch(actions.addEvent({username: 'TestUser', columnId: 0, begin: 0, end: 1}))
+            defaultStore.dispatch(actions.removeEvent({username: 'TestUser', columnId: 0, index: 0}))
+
+            expect(selectors.getEvents(defaultStore.getState(), 'TestUser', 0)).toEqual([])
         })
     })
-
-
 })
