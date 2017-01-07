29a8f8fc0d73e4a4b41ffd629d04b31b8aef8842
beac0n <mcschempp@gmx.de>
2017-01-08 00:52:08 +0100

    add tests

--- src/redux/controllers.test.js
+++ src/redux/controllers.test.js
@@ -8,20 +8,36 @@ describe('controllers', () => {
     beforeEach(() => defaultStore = initStore())
 
     const username = 'TestUser'
+    const otherTestUser = 'otherTestUser'
+
+    it('should add two events', () => {
+        defaultStore.dispatch(actions.addUser(username))
+        defaultStore.dispatch(actions.addUser(otherTestUser))
+        controllers.addEvent({username, columnId: 0, begin: 10, end: 15})(defaultStore.dispatch, defaultStore.getState)
+        controllers.addEvent({username, columnId: 0, begin: 15, end: 18})(defaultStore.dispatch, defaultStore.getState)
+
+        expect(selectors.getEvents(defaultStore.getState(), username, 0).length).toBe(2)
+        expect(selectors.hasInvalidEventAdd(defaultStore.getState(), username)).toBe(false)
+    })
+
     it('should not add overlapping event which starts before old event', () => {
         defaultStore.dispatch(actions.addUser(username))
+        defaultStore.dispatch(actions.addUser(otherTestUser))
         controllers.addEvent({username, columnId: 0, begin: 10, end: 15})(defaultStore.dispatch, defaultStore.getState)
         controllers.addEvent({username, columnId: 0, begin: 8, end: 12})(defaultStore.dispatch, defaultStore.getState)
 
         expect(selectors.getEvents(defaultStore.getState(), username, 0).length).toBe(1)
+        expect(selectors.hasInvalidEventAdd(defaultStore.getState(), username)).toBe(true)
     })
 
     it('should not add overlapping event which ends after old event', () => {
         defaultStore.dispatch(actions.addUser(username))
+        defaultStore.dispatch(actions.addUser(otherTestUser))
         controllers.addEvent({username, columnId: 0, begin: 10, end: 15})(defaultStore.dispatch, defaultStore.getState)
         controllers.addEvent({username, columnId: 0, begin: 12, end: 17})(defaultStore.dispatch, defaultStore.getState)
 
         expect(selectors.getEvents(defaultStore.getState(), username, 0).length).toBe(1)
+        expect(selectors.hasInvalidEventAdd(defaultStore.getState(), username)).toBe(true)
     })
 
     it('should not add including event', () => {
@@ -30,6 +46,7 @@ describe('controllers', () => {
         controllers.addEvent({username, columnId: 0, begin: 12, end: 14})(defaultStore.dispatch, defaultStore.getState)
 
         expect(selectors.getEvents(defaultStore.getState(), username, 0).length).toBe(1)
+        expect(selectors.hasInvalidEventAdd(defaultStore.getState(), username)).toBe(true)
     })
 
     it('should not add surrounding event', () => {
@@ -38,5 +55,6 @@ describe('controllers', () => {
         controllers.addEvent({username, columnId: 0, begin: 8, end: 17})(defaultStore.dispatch, defaultStore.getState)
 
         expect(selectors.getEvents(defaultStore.getState(), username, 0).length).toBe(1)
+        expect(selectors.hasInvalidEventAdd(defaultStore.getState(), username)).toBe(true)
     })
 })
--- src/redux/reducer/body.test.js
+++ src/redux/reducer/body.test.js
@@ -7,61 +7,108 @@ describe('body reducer', () => {
     let defaultStore
     beforeEach(() => defaultStore = initStore())
 
+    const testUser = 'TestUser'
+    const otherTestUser = 'otherTestUser'
+
     describe('user', () => {
         it('should add one user', () => {
-            defaultStore.dispatch(actions.addUser('TestUser'))
+            defaultStore.dispatch(actions.addUser(testUser))
 
-            expect(selectors.getUsers(defaultStore.getState())).toContain('TestUser')
+            expect(selectors.getUsers(defaultStore.getState())).toContain(testUser)
 
             expect(selectors.getUsers(defaultStore.getState()).length).toBe(1)
         })
 
         it('should add 2 user', () => {
-            defaultStore.dispatch(actions.addUser('TestUser'))
-            defaultStore.dispatch(actions.addUser('AnotherUser'))
+            defaultStore.dispatch(actions.addUser(testUser))
+            defaultStore.dispatch(actions.addUser(otherTestUser))
 
-            expect(selectors.getUsers(defaultStore.getState())).toContain('TestUser')
-            expect(selectors.getUsers(defaultStore.getState())).toContain('AnotherUser')
+            expect(selectors.getUsers(defaultStore.getState())).toContain(testUser)
+            expect(selectors.getUsers(defaultStore.getState())).toContain(otherTestUser)
 
             expect(selectors.getUsers(defaultStore.getState()).length).toBe(2)
+        })
+
+        it('should add 2 users and remove one user', () => {
+            defaultStore.dispatch(actions.addUser(testUser))
+            defaultStore.dispatch(actions.addUser(otherTestUser))
+
+            expect(selectors.getUsers(defaultStore.getState())).toContain(testUser)
+            expect(selectors.getUsers(defaultStore.getState())).toContain(otherTestUser)
+
+            defaultStore.dispatch(actions.removeUser(otherTestUser))
+
+            expect(selectors.getUsers(defaultStore.getState()).length).toBe(1)
+            expect(selectors.getUsers(defaultStore.getState())[0]).toBe(testUser)
         })
     })
 
     describe('event', () => {
-        it('should have no initial events', () => {
-            defaultStore.dispatch(actions.addUser('TestUser'))
 
-            expect(selectors.getEvents(defaultStore.getState(), 'TestUser', 0)).toEqual([])
+        it('should get the correct velocity', () => {
+            defaultStore.dispatch(actions.addUser(testUser))
+
+            expect(selectors.getUserVelocity(defaultStore.getState(), testUser)).toBe(8)
+        })
+
+        it('should get the correct velocity with 1 day', () => {
+            defaultStore.dispatch(actions.addUser(testUser))
+            defaultStore.dispatch(actions.addEvent({username: testUser, columnId: 0, begin: 10, end: 18}))
+
+            expect(selectors.getUserVelocity(defaultStore.getState(), testUser)).toBe(7)
+        })
+
+        it('should get the correct velocity with 2 days', () => {
+            defaultStore.dispatch(actions.addUser(testUser))
+            defaultStore.dispatch(actions.addEvent({username: testUser, columnId: 0, begin: 10, end: 18}))
+            defaultStore.dispatch(actions.addEvent({username: testUser, columnId: 11, begin: 10, end: 18}))
+
+            expect(selectors.getUserVelocity(defaultStore.getState(), testUser)).toBe(6)
+        })
+
+        it('should get the correct velocity with 3 days', () => {
+            defaultStore.dispatch(actions.addUser(testUser))
+            defaultStore.dispatch(actions.addEvent({username: testUser, columnId: 0, begin: 10, end: 18}))
+            defaultStore.dispatch(actions.addEvent({username: testUser, columnId: 11, begin: 10, end: 18}))
+            defaultStore.dispatch(actions.addEvent({username: testUser, columnId: 4, begin: 10, end: 18}))
+
+            expect(selectors.getUserVelocity(defaultStore.getState(), testUser)).toBe(5)
+        })
+
+        it('should have no initial events', () => {
+            defaultStore.dispatch(actions.addUser(testUser))
+
+            expect(selectors.getEvents(defaultStore.getState(), testUser, 0)).toEqual([])
         })
 
         it('should add an event', () => {
-            defaultStore.dispatch(actions.addUser('TestUser'))
-            defaultStore.dispatch(actions.addEvent({username: 'TestUser', columnId: 0, begin: 0, end: 1}))
+            defaultStore.dispatch(actions.addUser(testUser))
+            defaultStore.dispatch(actions.addEvent({username: testUser, columnId: 0, begin: 0, end: 1}))
 
-            expect(selectors.getEvents(defaultStore.getState(), 'TestUser', 0)).toContainEqual({begin: 0, end: 1})
+            expect(selectors.getEvents(defaultStore.getState(), testUser, 0)).toContainEqual({begin: 0, end: 1})
         })
 
         it('should add an event which is only one hour long', () => {
-            defaultStore.dispatch(actions.addUser('TestUser'))
-            defaultStore.dispatch(actions.addEvent({username: 'TestUser', columnId: 0, begin: 0}))
+            defaultStore.dispatch(actions.addUser(testUser))
+            defaultStore.dispatch(actions.addEvent({username: testUser, columnId: 0, begin: 0}))
 
-            expect(selectors.getEvents(defaultStore.getState(), 'TestUser', 0)).toContainEqual({begin: 0, end: 1})
+            expect(selectors.getEvents(defaultStore.getState(), testUser, 0)).toContainEqual({begin: 0, end: 1})
         })
 
         it('should update an event with a note', () => {
-            defaultStore.dispatch(actions.addUser('TestUser'))
-            defaultStore.dispatch(actions.addEvent({username: 'TestUser', columnId: 0, begin: 0, end: 1}))
-            defaultStore.dispatch(actions.updateEvent({username: 'TestUser', columnId: 0, index: 0, note: 'testNote'}))
+            defaultStore.dispatch(actions.addUser(testUser))
+            defaultStore.dispatch(actions.addEvent({username: testUser, columnId: 0, begin: 0, end: 1}))
+            defaultStore.dispatch(actions.updateEvent({username: testUser, columnId: 0, index: 0, note: 'testNote'}))
 
-            expect(selectors.getEvents(defaultStore.getState(), 'TestUser', 0)).toContainEqual({begin: 0, end: 1, note: 'testNote'})
+            expect(selectors.getEvents(defaultStore.getState(), testUser, 0)).toContainEqual({begin: 0, end: 1, note: 'testNote'})
         })
 
         it('should remove an event', () => {
-            defaultStore.dispatch(actions.addUser('TestUser'))
-            defaultStore.dispatch(actions.addEvent({username: 'TestUser', columnId: 0, begin: 0, end: 1}))
-            defaultStore.dispatch(actions.removeEvent({username: 'TestUser', columnId: 0, index: 0}))
+            defaultStore.dispatch(actions.addUser(testUser))
+            defaultStore.dispatch(actions.addEvent({username: testUser, columnId: 0, begin: 0, end: 1}))
+            defaultStore.dispatch(actions.removeEvent({username: testUser, columnId: 0, index: 0}))
 
-            expect(selectors.getEvents(defaultStore.getState(), 'TestUser', 0)).toEqual([])
+            expect(selectors.getEvents(defaultStore.getState(), testUser, 0)).toEqual([])
         })
     })
 })
