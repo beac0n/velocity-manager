cdcf3f59cefc305edbe792415c7755212f70c353
beac0n <mcschempp@gmx.de>
2017-01-15 19:52:14 +0100

    fix router bug

--- src/components/appRouter.js
+++ src/components/appRouter.js
@@ -5,7 +5,7 @@ import TeamSelector from './teamSelector/teamSelector'
 
 const AppRouter = () => (
     <Router history={browserHistory}>
-        <Route path="/app/:teamName" component={App}/>
+        <Route path="./app/:teamName" component={App}/>
         <Route path="*" component={TeamSelector} />
     </Router>)
 
