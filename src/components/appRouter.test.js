6044cd776a0c58db6af92251e969478197e6aac0
beac0n <mcschempp@gmx.de>
2017-01-21 13:08:10 +0100

    add render test stub

---
+++ src/components/appRouter.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+//import {shallow} from 'enzyme'
+import {initStore} from '../redux/init'
+import AppRouter from './appRouter'
+
+describe('<AppRouter />', () => {
+    it('should render', () => {
+//        const wrapper = shallow(<AppRouter />);
+    })
+
+})
